2c82b9c61b436c9a4895876942eaf0f88f4435ea
Arthur Cordova Stapassoli <arthur.stapassoli@gmail.com>
2021-09-08 18:58:28 -0300

    Initial commit

---
+++ app/src/main/java/com/arcanesecurity/nasalighting/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.arcanesecurity.nasalighting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.arcanesecurity.nasalighting.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
